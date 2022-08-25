ed1f27b521164fb9436564ffc887a4b598bdc90a
Roman Sermyagin <sermyagin_ra@nsdi.rosreestr.ru>
2022-08-25 19:26:54 +0300

    Учтены замечания ментора: - в классы моделей добавлены содержательные комментарии и оформлены в виде """docstrings"""; - модели распределены по приложениям accounts и posts.

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,13 @@
-from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
-from accounts.resources import POST_TYPE
+# from posts.models import Post, Comment 
 
 class Author(models.Model):
+    """
+    Author: модель, содержащая объекты всех авторов
+    user: связь o2o со встроенной моделью User
+    update_rating: вычисление рейтинга пользователя
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     _rating = models.IntegerField(default=0)
 
@@ -17,6 +21,7 @@ class Author(models.Model):
         self.save()
     
     def update_rating(self):
+        from posts.models import Comment, Post
         self._rating = 0
         for comment in Comment.objects.filter(user = self.user):
             self._rating += comment.rating
@@ -25,68 +30,4 @@ class Author(models.Model):
             self._rating += post.rating * 3
             for comments_to_post in Comment.objects.filter(post = post):
                 self._rating += comments_to_post.rating
-
-        self.save()
-
-class Category(models.Model):
-    category_name = models.CharField(max_length=255, unique=True)
-
-class Post(models.Model):
-    paper = 'PA'
-    news = 'NE'
-
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    post_type = models.CharField(max_length=2, choices=POST_TYPE, default=paper)
-    pub_time = models.DateTimeField(auto_now_add=True)
-    categories = models.ManyToManyField(Category, through='PostCategory')
-    title = models.CharField(max_length=255, default='Введите название')
-    text = models.TextField(default='Введите содержание')
-    _rating = models.IntegerField(default=0)
-
-    @property
-    def rating(self):
-        return self._rating
-
-    @rating.setter
-    def rating(self, value):
-        self._rating = int(value) if value >=0 else 0
-        self.save()
-
-    def like(self):
-        self._rating += 1
-        self.save()
-    
-    def dislike(self):
-        self._rating += -1
-        self.save()
-        
-    def preview(self):
-        return f'{self.text[0:124]}...'
-
-class PostCategory(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.TextField(default='Введите комментарий')
-    pub_time = models.DateTimeField(auto_now_add=True)
-    _rating = models.IntegerField(default=0)
-
-    @property
-    def rating(self):
-        return self._rating
-
-    @rating.setter
-    def rating(self, value):
-        self._rating = int(value) if value >=0 else 0
-        self.save()
-
-    def like(self):
-        self._rating += 1
-        self.save()
-    
-    def dislike(self):
-        self._rating += -1
         self.save()
---
+++ posts/models.py
@@ -0,0 +1,95 @@
+from django.db import models
+from django.contrib.auth.models import User
+from accounts.models import Author 
+from accounts.resources import POST_TYPE
+
+class Category(models.Model):
+    """
+    Category: категории новостей/статей
+    """
+    category_name = models.CharField(max_length=255, unique=True)
+
+class Post(models.Model):
+    """
+    Post: модель содержит в себе статьи и новости
+    author: o2m связь с моделью Author
+    post_type: тип публикации - статья или новость
+    put_time: автоматическая дата и время публикации
+    categories: o2m связь с моделью Category
+    title: заголовок
+    text: текст
+    _rating: рейтинг, который определяется количеством лайков
+    like|dislike: методы, которые увеличивают/уменьшают рейтинг
+    """
+    paper = 'PA'
+    news = 'NE'
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    post_type = models.CharField(max_length=2, choices=POST_TYPE, default=paper)
+    pub_time = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=255, default='Введите название')
+    text = models.TextField(default='Введите содержание')
+    _rating = models.IntegerField(default=0)
+
+    @property
+    def rating(self):
+        return self._rating
+
+    @rating.setter
+    def rating(self, value):
+        self._rating = int(value) if value >=0 else 0
+        self.save()
+
+    def like(self):
+        self._rating += 1
+        self.save()
+    
+    def dislike(self):
+        self._rating += -1
+        self.save()
+        
+    def preview(self):
+        return f'{self.text[0:124]}...'
+
+class PostCategory(models.Model):
+    """
+    PostCategory: промежуточная модель для m2m связи
+    post: o2m связь с моделью Post
+    category: o2m связь с моделью Category
+    """
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    """
+    Comment: модель для комментариев к статье или новости
+    post: o2m связь с моделью Post
+    user: o2m связь с моделью User
+    text: текст комментария
+    put_time: дата и время публикации
+    _rating: рейтинг, который определяется количеством лайков
+    like|dislike: методы, которые увеличивают/уменьшают рейтинг
+    """
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(default='Введите комментарий')
+    pub_time = models.DateTimeField(auto_now_add=True)
+    _rating = models.IntegerField(default=0)
+
+    @property
+    def rating(self):
+        return self._rating
+
+    @rating.setter
+    def rating(self, value):
+        self._rating = int(value) if value >=0 else 0
+        self.save()
+
+    def like(self):
+        self._rating += 1
+        self.save()
+    
+    def dislike(self):
+        self._rating += -1
+        self.save()
