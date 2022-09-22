6aee573e92bbfe0863d80d6ae3c28ad782372f9d
roman <roman@geod.ru>
2022-09-22 18:11:34 +0300

    modified:   NewsPaper/__init__.py 	modified:   NewsPaper/celery.py

--- NewsPaper/celery.py
+++ NewsPaper/celery.py
@@ -1,11 +1,9 @@
-from argparse import Namespace
 import os
 from celery import Celery
-
-
+ 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPaper.settings')
-
+ 
 app = Celery('posts')
-app.config_from_object('django.conf:setting', namespace = 'CELERY')
+app.config_from_object('django.conf:settings', namespace = 'CELERY')
 
 app.autodiscover_tasks()
